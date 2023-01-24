6f9f22cfeecf4a85118eafa5a6a5170081646e7d
Irina Gorynina <iren.gorynina@gmail.com>
2023-01-24 15:18:08 +0300

    решение 41 задачи

--- Exercise41/Program.cs
+++ Exercise41/Program.cs
@@ -1,64 +1,15 @@
 ﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-Console.Write("Введите числа через запятую: ");
-int[] numbers = StringToNum(Console.ReadLine());
-PrintArray(numbers);
-int sum = 0;
+Console.Write("Введите числа через пробел: ");
+int[] numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
 for (int i = 0; i < numbers.Length; i++)
 {
     if (numbers[i] > 0)
     {
-        sum++;
+        count++;
     }
 }
-Console.WriteLine();
-Console.WriteLine($"Числа больше 0 = {sum}");
-
-
-int[] StringToNum(string input)
-{
-    int count = 1;
-    for (int i = 0; i < input.Length; i++)
-    {
-        if (input[i] == ',')
-        {
-            count++;
-        }
-    }
-
-    int[] numbers = new int [count];
-    int index = 0;
-
-    for (int i = 0; i < input.Length; i++)
-    {
-        string temp = "";
-
-        while (input [i] != ',')
-        {
-        if(i != input.Length - 1)
-        {
-            temp += input [i].ToString();
-            i++;
-        }
-        else
-        {
-            temp += input [i].ToString();
-            break;
-        }
-        }
-        numbers[index] = Convert.ToInt32(temp);
-        index++;
-    }
-    return numbers;
-}
-
-
-void PrintArray(int[] array)
-{
-    Console.Write("[ ");
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-    Console.Write("]");
-}
+ 
+Console.WriteLine($"Количество чисел больше 0: {count}");
